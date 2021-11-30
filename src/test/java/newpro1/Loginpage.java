1b9e94201a135d884e0a59b40af508d7c184b152
KesavanAK <kersavanak777@gmail.com>
2021-11-30 11:55:11 +0530

    Sha's code

--- src/test/java/newpro1/Loginpage.java
+++ src/test/java/newpro1/Loginpage.java
@@ -4,6 +4,9 @@ public class Loginpage {
 	
 	 public static void main(String[] args) {
 		 System.out.println("---------WeLcOmE New pRojEcT--------");
+		 System.out.println("*****Sha1*****");
+		 System.out.println("*****Sha2*****");
+		 
 		
 	}
 	}
